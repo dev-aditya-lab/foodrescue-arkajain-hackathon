b0c9af46226ae8452f3d2564fd33d22e40eda4e1
dev-aditya-lab <ad1123itya@gmail.com>
2026-04-10 16:17:58 +0530

    Add authentication middleware to verify JWT token and identify user

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,17 @@
+import { JWT_SECRET } from "../config/env.config.js";
+
+export async function identifyUser(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.user = decoded.userId
+        req.role = decoded.role
+        next();
+    } catch (error) {
+        console.error('Error verifying token:', error);
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+}
